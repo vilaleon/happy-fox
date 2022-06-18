2b2921099a59124a0ccc975b92fdf96d17865553
Leon Vila <monnochromo@gmail.com>
2022-06-18 12:55:57 +0200

    Update title scene
    In this update, following things are added:
    
     - New font 'Eczar'
     - Title scene animations
     - Script for handling button clicks

--- Assets/Scripts/Title Screen/CameraFollowEffect.cs
+++ Assets/Scripts/Title Screen/CameraFollowEffect.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class CameraFollowEffect : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private new Camera camera;
+    [SerializeField] private float transformMultiplier;
+
+    private Vector3 startingPosition;
+    public Vector3 mousePositon;
+
     void Start()
     {
-        
+        startingPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        mousePositon = camera.ScreenToViewportPoint(Input.mousePosition) + new Vector3(-0.5f,-0.5f);
+        mousePositon.x = mousePositon.x > 0.5f ? 0.5f : mousePositon.x;
+        mousePositon.x = mousePositon.x < -0.5f ? -0.5f : mousePositon.x;
+        mousePositon.y = mousePositon.y > 0.5f ? 0.5f : mousePositon.y;
+        mousePositon.y = mousePositon.y < -0.5f ? -0.5f : mousePositon.y;
+        transform.position = startingPosition + mousePositon * transformMultiplier;
     }
 }
