fab2cd393fa3cafd1a65b2657da3c795b36d1d56
Leon Vila <monnochromo@gmail.com>
2022-06-26 10:20:32 +0200

    Add audio mixer
    Audio Mixer added to the project. It has two channels, one for Music volume, and one for SFX volume. In-game, these are controller through sliders in settings menu.
    
    Resolves: #7

--- Assets/Scripts/GameMenuUIHandler.cs
+++ Assets/Scripts/GameMenuUIHandler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class GameMenuUIHandler : MonoBehaviour
